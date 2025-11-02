70eeffbf1e07b3121ee59f74a7b06bd702336203
Shruti@2050 <shrutijoshi1208@gmail.com>
2025-11-02 13:20:43 +0530

    Find first and last position of targeted element

---
+++ Arrays/Findfirst_last_6.java
@@ -0,0 +1,42 @@
+class Findfirst_last_6 {
+   
+    private int findFirst(int[] nums, int target){
+        int left = 0; int right = nums.length-1 ; int index = -1;
+         while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+            if (nums[mid] == target) index = mid;
+        }
+        return index;
+    }
+     private int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, index = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+            if (nums[mid] == target) index = mid;
+        }
+        return index;
+    }
+
+     public static void main(String args[]) {
+        int nums []= {5,7,7,8,8,10};
+        int target = 8;
+        Findfirst_last_6 obj = new Findfirst_last_6();
+        int result[] = obj.searchRange (nums, target);
+        System.out.println("First and Last Position of Element is: [" + result[0] + ", " + result[1] + "]");
+    }
+    public int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums,target);
+        int last = findLast(nums,target);
+        return new int[]{first, last};
+    }
+}
