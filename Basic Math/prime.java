8e399a92229ab56e9f24571c22f1f35035dea3d1
Shruti@2050 <shrutijoshi1208@gmail.com>
2026-01-15 14:50:37 +0530

    -

--- Basic Math/prime.java
+++ Basic Math/prime.java
@@ -18,7 +18,7 @@ class prime{
         // }
 
         // optimized approach
-        for(int i =1; i*i<= n; i++){
+        for(int i =1; i<= Math.sqrt(n); i++){
             if(n % i == 0){
                 count++;
                 if(i != n / i){
